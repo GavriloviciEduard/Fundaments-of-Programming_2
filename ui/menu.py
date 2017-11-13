3ffd56e2f69bdf76d131564058eccf5bde60b4a9
Gavrilovici Eduard <33176335+GavriloviciEduard@users.noreply.github.com>
2017-11-13 17:32:31 +0200

    Update menu.py

--- ui/menu.py
+++ ui/menu.py
@@ -5,6 +5,8 @@ import ui.in_out_functions
 
 NULL='NULL'
 
+repo=[]
+
 def menu():    
     
     """
@@ -13,11 +15,15 @@ def menu():
             OUT: -
             DESC: The function creates a menu.
     """
+    
+    global repo
+    repo=ui.in_out_functions.read_repo()
+    
     while True:
 
         os.system("cls")
 
-        print("1.Add a scalar to a vector.")
+        print("1.Add a scalar to a vector(the read vector is added to the repository).")
         print("2.Add two vectors.")
         print("3.Subtract two vectors.")
         print("4.Multiplication.")
@@ -32,70 +38,94 @@ def menu():
 
         if opt=='1':
             test.apd.test_add_scalar_and_vector()
-            v=ui.in_out_functions.read_vector()
             s=ui.in_out_functions.read_scalar()
-            rez=domain.vec_o.add_scalar_and_vector(v,s)
-            print(v,"+",s,"=",rez)
+            v=ui.in_out_functions.read_vector()
+            repo.append(v)
+            for v in repo:
+                rez=domain.vec_o.add_scalar_and_vector(v,s)
+                print(v,"+",s,"=",rez)
 
         elif opt=='2':
             test.apd.test_add_vectors()
             v1=ui.in_out_functions.read_vector()
-            v2=ui.in_out_functions.read_vector()
-            rez=domain.vec_o.add_vectors(v1, v2)
-            if rez!=NULL:
-                print(v1,"+",v2,"=",rez)
-            else:
-                print("Operation failed!")
+            for v2 in repo:
+                rez=domain.vec_o.add_vectors(v1, v2)
+                if rez!=NULL:
+                    print(v1,"+",v2,"=",rez)
+                else:
+                    print("Operation failed!")
         
         elif opt=='3':
             test.apd.test_sub_vectors()
             v1=ui.in_out_functions.read_vector()
-            v2=ui.in_out_functions.read_vector()
-            rez=domain.vec_o.sub_vectors(v1, v2)
-            if rez!=NULL:
-                print(v1,"-",v2,"=",rez)
-            else:
-                print("Operation failed!")
+            for v2 in repo:
+                rez=domain.vec_o.sub_vectors(v1, v2)
+                if rez!=NULL:
+                    print(v1,"-",v2,"=",rez)
+                else:
+                    print("Operation failed!")
         
         elif opt=='4':
             test.apd.test_mult_vectors()
             v1=ui.in_out_functions.read_vector()
-            v2=ui.in_out_functions.read_vector()
-            rez=domain.vec_o.mult_vectors(v1, v2)
-            if rez!=NULL:
-                print(v1,"*",v2,"=",rez)
-            else:
-                print("Operation failed!")
+            for v2 in repo:
+                rez=domain.vec_o.mult_vectors(v1, v2)
+                if rez!=NULL:
+                    print(v1,"*",v2,"=",rez)
+                else:
+                    print("Operation failed!")
                 
         elif opt=='5':
             test.apd.test_sum_vector()
             v=ui.in_out_functions.read_vector()
             rez=domain.vec_o.sum_vector(v)
             print("The sum of",v,"is",rez)
+            for v in repo:
+                rez=domain.vec_o.sum_vector(v)
+                print("The sum of",v,"is",rez)
+                
             
         elif opt=='6':
             test.apd.test_prod_vector()
             v=ui.in_out_functions.read_vector()
             rez=domain.vec_o.prod_vector(v)
             print("The product of",v,"is",rez)
+            for v in repo:
+                rez=domain.vec_o.prod_vector(v)
+                print("The product of",v,"is",rez)
+                
             
         elif opt=='7':
             test.apd.test_avg_vector()
             v=ui.in_out_functions.read_vector()
             rez=domain.vec_o.avg_vector(v)
             print("The average of",v,"is",rez)
+            for v in repo:
+                rez=domain.vec_o.avg_vector(v)
+                print("The average of",v,"is",rez)
+                
+                
             
         elif opt=='8':
             test.apd.test_min_vector()
             v=ui.in_out_functions.read_vector()
             rez=domain.vec_o.min_vector(v)
             print("The minimum of",v,"is",rez)
+            for v in repo:
+                rez=domain.vec_o.min_vector(v)
+                print("The minimum of",v,"is",rez)
+                
             
         elif opt=='9':
             test.apd.test_max_vector()
             v=ui.in_out_functions.read_vector()
             rez=domain.vec_o.max_vector(v)
             print("The maximum of",v,"is",rez)
+            for v in repo:
+                rez=domain.vec_o.max_vector(v)
+                print("The maximum of",v,"is",rez)
+                
+            
         
         elif opt=='0':
             break
