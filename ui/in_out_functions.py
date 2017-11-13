e6997ee0da2486d6d35b2a23977f4d9a91a9b074
Gavrilovici Eduard <33176335+GavriloviciEduard@users.noreply.github.com>
2017-11-13 17:32:00 +0200

    Update in_out_functions.py

--- ui/in_out_functions.py
+++ ui/in_out_functions.py
@@ -1,4 +1,26 @@
 
+def read_repo():
+    
+    """
+    read_repo=>
+                input:-
+                output: A list of lists of integers 'repo'.
+                desc.: The function reads and returns a list of lists of integers 'repo'.
+    """
+    
+    repo=[]
+    
+    
+    print("Give the number of vectors you want to be read=>")
+    n=read_scalar()
+    
+    while n:
+        repo.append(read_vector())
+        n-=1
+        
+    return repo
+    
+
 def read_vector():
     
     """
@@ -47,7 +69,7 @@ def read_scalar():
     
     while True:
         try:
-            s=int(input("Give a scalar:"))
+            s=int(input("Give a number:"))
             return s
         except ValueError:
             print("Not a valid value! Try again!")
